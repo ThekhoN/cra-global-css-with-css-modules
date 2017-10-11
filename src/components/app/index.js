cadc4fcce478d68db207bcfbc668a8901fe79ac7
ThekhoN <bennetonss@hotmail.com>
2017-10-11 14:27:14 +0530

    first

---
+++ src/components/app/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./style.css";
+
+const App = () => (
+  <div className={styles.mainApp}>
+    <h2>This is the main App</h2>
+  </div>
+);
+
+export default App;
